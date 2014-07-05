1b93bb752ff5df445a13d947d103fa9cabb2169a
Ben Nelson <s.benjamin.nelson@gmail.com>
2014-07-05 12:11:55 -0600

    I implemented the filter functionality.  When you click on a user's name the tweets are filtered by that user's tweets.

--- main.js
+++ main.js
@@ -1,19 +1,43 @@
 $(document).ready(function(){
 
+
   function populate(){
     $('li.tweetClass').remove();
     for(var i = 0; i < streams.home.length; i++){
       var tweet = streams.home[i];
       var userName = '@' + tweet.user;
-      $('ul.tweets').append('<li class=" tweetClass list-group-item">' + '<p id="author">' + userName + '  ' + tweet.created_at + '</p>' + tweet.message + '</li>');
+      $('ul.tweets').append('<li class="tweetClass list-group-item">' + '<p id="author">' + '<div class="filterValue">' + userName + '</div>' + '  ' + tweet.created_at + '</p>' + tweet.message + '</li>');
     }
-  }
+  };
 
-  var displayTweets = function(){
-    console.log(streams.home[1].message);
-  }
+  function userPopulate(input){
+    $('li.tweetClass').remove();
+    for(var i = 0; i < streams.home.length; i++){
+      var tweet = streams.home[i];
+      var userName = '@' + tweet.user;
+      if(input === userName){
+      $('ul.tweets').append('<li class="tweetClass list-group-item">' + '<p id="author">' + '<div class="filterValue">' + userName + '</div>' + '  ' + tweet.created_at + '</p>' + tweet.message + '</li>');
+      }
+    }
+  };
 
+  var refreshSwitch = true;
   populate();
-  setInterval(populate, 3000);
+  setInterval(function(){
+    if(refreshSwitch === true){
+      populate();
+    }
+
+  }, 3000);
+
+  $(document).on('click','.filterValue', function(){
+    refreshSwitch = false;
+    userPopulate($(this).text());
+  });
+
+  $('#title').on('click', function(){
+    refreshSwitch = true;
+    populate();
+    });
 
 });
